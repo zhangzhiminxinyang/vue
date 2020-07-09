f6eded706ae8b25a27bdeaa98e6ee2e10781d88b
zhangzhiminxinyang <363787211@qq.com>
2020-07-09 19:43:12 +0800

    定义并显示变量

--- code/hello/src/components/SayHi.vue
+++ code/hello/src/components/SayHi.vue
@@ -1,13 +1,15 @@
 <template>
     <div>
-      Hi Vue!
+      Hi Vue!{{message}}
     </div>
 </template>
 
 <script>
     export default {
         data(){
-          return {}
+          return {
+            message:'在data中定义变量,在html部分显示变量的值'
+          }
         },
         name: "SayHi"
     }
