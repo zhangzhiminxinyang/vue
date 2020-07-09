d943df2bc4ff135092e9a94a68fc200cc18f298e
zhangzhiminxinyang <363787211@qq.com>
2020-07-09 19:24:33 +0800

    在 Hello中添加SayHi组件

---
+++ code/hello/src/components/SayHi.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+      Hi Vue!
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+          return {}
+        },
+        name: "SayHi"
+    }
+</script>
+
+<style scoped>
+
+</style>
